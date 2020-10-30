c8ec21ada2d5c9e51c1139de8afb1e099821a3a5
ABHINAV TOMAR <atomar.cse18@chitkarauniversity.edu.in>
2020-10-30 19:03:45 +0530

    App Changes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const db = require("./configs/database");
 
 app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -32,10 +34,14 @@ app.use('/', require('./routes/index'));
 app.use('/', require('./routes/settings'));
 app.use('/h/', require('./routes/hub'));
 app.use('/u/', require('./routes/profile'));
-app.use('/api', require('./routes/api'));
+app.use('/api/', require('./routes/api'));
 
 
 app.get('*', function (req, res) {
     res.status(404);
-    res.render("./error")
+    res.send("error")
+});
+
+app.listen(process.env.PORT || 5000, function () {
+    console.log("listening on port 5000!");
 });
